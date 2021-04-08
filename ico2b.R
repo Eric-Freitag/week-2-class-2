fb5cbb100a303cf5744ebfd85e136e93782fe801
Eric Freitag <ehfreitag87@gmail.com>
2021-04-08 14:27:31 -0700

    add a script that computes light year

---
+++ ico2b.R
@@ -0,0 +1,8 @@
+c<- 299792.458
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightDay <- lightHour*24
+lightYear<- lightDay*365.2422
+cat("length of light year in meters:", lightYear, "\n.", sep=" ")
+cat("That's long enough")
+
